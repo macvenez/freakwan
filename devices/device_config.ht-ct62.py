a55f79256960ebd058096daedd451688b9917da6
macvenez <macvenez@gmail.com>
2024-09-18 16:45:56 +0200

    Added config for htct62

---
+++ devices/device_config.ht-ct62.py
@@ -0,0 +1,24 @@
+### Heltec HT-CT62 hardware configuration
+
+from machine import Pin, ADC
+import time
+
+class DeviceConfig:
+    config = {}
+
+    def power_up(freakwan):
+        return
+
+    def get_battery_microvolts():
+        return 3300000
+
+    # Pin configuration for the SX1262.
+    config['sx1262'] = {
+        'busy': 4,
+        'miso': 6,
+        'mosi': 7,
+        'clock': 10,
+        'chipselect': 8,
+        'reset': 5,
+        'dio': 3,
+    }
